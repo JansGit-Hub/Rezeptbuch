7ec95cde2da133208baadb090cdb539a55c0d9ad
janbecker <jan1508@me.com>
2025-05-07 13:43:33 +0200

    [added] NavigationMenu

--- src/main/java/com/rezeptbuch/view/NavigationMenu.java
+++ src/main/java/com/rezeptbuch/view/NavigationMenu.java
@@ -2,7 +2,7 @@ package com.rezeptbuch.view;
 
 import com.vaadin.flow.component.sidenav.SideNav;
 import com.vaadin.flow.component.sidenav.SideNavItem;
-
+//Test
 public class NavigationMenu extends SideNav {
     public NavigationMenu() {
         addItem(new SideNavItem("Startseite", OverView.class));
